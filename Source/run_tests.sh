19aae16c73167952e1560b51e0ce0f74356a6785
Ralph <erkamps12@gmail.com>
2020-01-23 13:13:33 +0100

    pull

---
+++ Source/run_tests.sh
@@ -0,0 +1,5 @@
+for f in ./out/*.png; do
+    echo "$f"
+    # do some stuff here with "$f"
+    # remember to quote it or spaces may misbehave
+done
---
+++ source/run_tests.sh
@@ -0,0 +1,4 @@
+for f in ./*.doc; do
+    # do some stuff here with "$f"
+    # remember to quote it or spaces may misbehave
+done
