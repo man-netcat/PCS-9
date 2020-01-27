b2b39f9d1c18f91b00f2add9f17b43d0a6f1134d
Ralph <erkamps12@gmail.com>
2020-01-27 14:22:40 +0100

    improved tests

--- source/run_tests.sh
+++ source/run_tests.sh
@@ -2,13 +2,13 @@ python3 gen_bifurcation.py
 
 var="1"
 for f in ./out/*.png; do
-    python3 blood_flow.py "$f" --out density"$var".mp4 --method density
+    python3 blood_flow.py "$f" video --out density"$var".mp4 --method density -l 100 -g density"$var".png
     var=$((var + 1))
 done
 
 var="1"
 for f in ./out/*.png; do
-    python3 blood_flow.py "$f" --out density"$var".mp4 --method velocity
+    python3 blood_flow.py "$f" video --out velocity"$var".mp4 --method velocity -l 100 -g velocity"$var".png
     var=$((var + 1))
 done
 
