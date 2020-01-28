9d316a20df21c1c5a1b71596adfda3f08cdc0aa2
Ralph <erkamps12@gmail.com>
2020-01-28 14:34:25 +0100

    Added comments and improved the testing script.

--- source/run_tests.sh
+++ source/run_tests.sh
@@ -1,15 +1,13 @@
 python3 gen_bifurcation.py
 
-var="1"
-for f in ./out/*.png; do
-    python3 blood_flow.py "$f" video --out density"$var".mp4 --method density -l 100 -g density"$var".png
-    var=$((var + 1))
-done
+mkdir ./density_videos ./density_graphs ./velocity_videos ./velocity_graphs
 
 var="1"
 for f in ./out/*.png; do
-    python3 blood_flow.py "$f" video --out velocity"$var".mp4 --method velocity -l 100 -g velocity"$var".png
+    python3 blood_flow.py "$f" video --out ./density_videos/density"$var".mp4 --method density -l 100 -g ./density_graphs/density"$var".png
+    python3 blood_flow.py "$f" video --out ./velocity_videos/velocity"$var".mp4 --method velocity -l 100 -g ./velocity_graphs/velocity"$var".png
     var=$((var + 1))
 done
 
 rm -r ./out
+echo "Tests completed!"
